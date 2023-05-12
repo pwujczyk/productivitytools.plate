1071bb199188e93b749d3f564a92bcc786828ce5
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-05-12 10:25:38 +0200

    initial value

--- src/components/PTPlate.tsx
+++ src/components/PTPlate.tsx
@@ -1,19 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Plate, TEditableProps } from "@udecode/plate";
-
+import { MyParagraphElement } from "./typescript/plateTypes";
 
 export interface IPTPlate extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLSpanElement>, HTMLSpanElement> {
-  label?: string;
+  content: string;
 }
 
-export const PTPlate: React.FunctionComponent<IPTPlate> = (props) => {
 
-    const editableProps: TEditableProps = {
-        placeholder: 'Type1...',
-      };
+const initialValue = (content: string) => [
+  {
+    type: "p",
+    children: [
+      {
+        text: content,
+      },
+    ],
+  } as MyParagraphElement,
+];
+
+export const PTPlate: React.FunctionComponent<IPTPlate> = ({ content }) => {
+  const [formatedValue, setFormatedValue] = useState<MyParagraphElement[]>(initialValue(content));
+
+  useEffect(() => {
+    setFormatedValue(initialValue(content));
+  }, [content]);
+
+  const editableProps: TEditableProps = {
+    placeholder: "Type1...",
+  };
   return (
     <p>
-      <Plate editableProps={editableProps} />
+      <Plate editableProps={editableProps} initialValue={formatedValue} />
     </p>
   );
 };
